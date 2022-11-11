ef6ca4d9d96e73b3312d2b49f35c2be5c27a8796
gaepsuni <105390436+gaepsuni@users.noreply.github.com>
2022-11-11 15:32:22 -0800

    Add avax contract addresses (#18)
    * Add contract addresses for avax mainnet
    
    * v0.2.0

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -4,7 +4,7 @@ export const DEX_V2_SUBGRAPH: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]:
     'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-fuji',
   [ChainId.AVALANCHE]:
-    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2 '
+    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2'
 }
 
 export const DEXCANDLES_SUBGRAPH_V2: { [chainId in ChainId]: string } = {
