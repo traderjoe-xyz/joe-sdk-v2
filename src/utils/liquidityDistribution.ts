05cc18afa9efc534a382c6c2a2fe85a2e2425d51
murloc <81496776+0xmurloc@users.noreply.github.com>
2023-02-19 00:13:50 +0100

    add custom bid-ask

--- src/utils/liquidityDistribution.ts
+++ src/utils/liquidityDistribution.ts
@@ -135,3 +135,118 @@ export const getUniformDistributionFromBinRange = (
     distributionY: _distributionY.map((el) => parseEther(el.toString()))
   }
 }
+
+/**
+ * Returns Bid-Ask distribution params for custom bin range
+ *
+ * @param {number} activeId
+ * @param {number[]} binRange
+ * @param {CurrencyAmount[]} parsedAmounts
+ * @returns
+ */
+export const getBidAskDistributionFromBinRange = (
+  activeId: number,
+  binRange: number[],
+  parsedAmounts: CurrencyAmount[]
+): LiquidityDistributionParams => {
+  const [parsedAmountA, parsedAmountB] = parsedAmounts
+
+  // init return values
+  let deltaIds: number[] = [],
+    _distributionX: number[] = [],
+    _distributionY: number[] = []
+
+  // range only includes B tokens (Y tokens)
+  if (binRange[1] <= activeId && parsedAmountA.raw.toString() === '0') {
+    const negDelta = binRange[1] - binRange[0] + 1
+    const negativeDeltaIds = Array.from(Array(activeId - binRange[0]).keys())
+      .reverse()
+      .slice(0, negDelta)
+      .map((el) => -1 * (el + 1))
+
+    deltaIds = [...negativeDeltaIds]
+    if (activeId === binRange[1]) {
+      deltaIds.push(0)
+    }
+
+    _distributionX = [...Array(deltaIds.length).fill(0)]
+
+    // dist = 2/R^2 * i
+    const rSquare = Math.pow(deltaIds[0], 2)
+    _distributionY = deltaIds.map((i) => (i * 2) / rSquare)
+  }
+
+  // range only includes A tokens (X tokens)
+  else if (activeId <= binRange[0] && parsedAmountB.raw.toString() === '0') {
+    const posDelta = binRange[1] - binRange[0] + 1
+    const positiveDeltaIds = Array.from(Array(binRange[1] - activeId).keys())
+      .reverse()
+      .slice(0, posDelta)
+      .reverse()
+      .map((el) => el + 1)
+
+    deltaIds = [...positiveDeltaIds]
+    if (activeId === binRange[0]) {
+      deltaIds.unshift(0)
+    }
+    // dist = 2/R^2 * i
+    const rSquare = Math.pow(deltaIds[deltaIds.length - 1], 2)
+    _distributionX = deltaIds.map((i) => (i * 2) / rSquare)
+    _distributionY = [...Array(deltaIds.length).fill(0)]
+  }
+
+  // range includes both X and Y tokens
+  else {
+    const negDelta = activeId - binRange[0]
+    const posDelta = binRange[1] - activeId
+
+    const negativeDeltaIds = Array.from(Array(negDelta).keys())
+      .reverse()
+      .map((el) => -1 * (el + 1))
+    const positiveDeltaIds = Array.from(Array(posDelta).keys()).map(
+      (el) => el + 1
+    )
+    deltaIds = [...negativeDeltaIds, 0, ...positiveDeltaIds]
+
+    // dist = 1/R^2 * i
+    const rSquare = Math.pow(deltaIds[0], 2)
+    _distributionX = [
+      ...Array(negDelta).fill(0),
+      0,
+      ...positiveDeltaIds.map((i) => i / rSquare)
+    ]
+    _distributionY = [
+      ...negativeDeltaIds.map((i) => i / rSquare)),
+      0,
+      ...Array(posDelta).fill(0)
+    ]
+  }
+
+  // return
+  return {
+    deltaIds,
+    distributionX: _distributionX.map((el) => parseEther(el.toString())),
+    distributionY: _distributionY.map((el) => parseEther(el.toString()))
+  }
+}
+
+/**
+ * Returns Curve distribution params for custom bin range
+ *
+ * @param {number} activeId
+ * @param {number[]} binRange
+ * @param {CurrencyAmount[]} parsedAmounts
+ * @returns
+ */
+export const getCurveDistributionFromBinRange = (
+  activeId: number,
+  binRange: number[],
+  parsedAmounts: CurrencyAmount[]
+): LiquidityDistributionParams => {
+  // TODO
+  return {
+    deltaIds: [],
+    distributionX: [],
+    distributionY: []
+  }
+}
