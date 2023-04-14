a36ed464a005993363d4df9c62452c1bf1073243
murloc <81496776+0xmurloc@users.noreply.github.com>
2023-04-14 13:17:44 +0200

    curve distribution

--- src/utils/liquidityDistribution.ts
+++ src/utils/liquidityDistribution.ts
@@ -243,11 +243,114 @@ export const getCurveDistributionFromBinRange = (
   binRange: number[],
   parsedAmounts: CurrencyAmount[]
 ): LiquidityDistributionParams => {
-  // TODO
-  console.log('[sdk:curve] ' + { activeId, binRange, parsedAmounts })
+  const [parsedAmountA, parsedAmountB] = parsedAmounts
+
+  // init return values
+  let deltaIds: number[] = [],
+    _distributionX: number[] = [],
+    _distributionY: number[] = []
+
+  // range only includes B tokens (Y tokens)
+  if (binRange[1] <= activeId && parsedAmountA.raw.toString() === '0') {
+    const negDelta = binRange[1] - binRange[0] + 1
+    const negativeDeltaIds = Array.from(Array(activeId - binRange[0]).keys())
+      .reverse()
+      .slice(0, negDelta)
+      .map((el) => -1 * (el + 1))
+
+    deltaIds = [...negativeDeltaIds]
+    if (activeId === binRange[1]) {
+      deltaIds.push(0)
+    }
+
+    _distributionX = [...Array(deltaIds.length).fill(0)]
+
+    // sigma = R / factor
+    const _R = deltaIds[0]
+    const factor = _R > 20 ? 2 : 1.5
+    const sigma = _R / factor
+
+    // A = 1 / (sigma  * sqrt(2 * pi))
+    const k = Math.pow(Math.PI * 2, -2)
+    const A = k / sigma
+
+    // dist = A * exp(-0.5 * (r /sigma) ^ 2)
+    _distributionY = deltaIds.map(
+      (i) => A * Math.exp(-0.5 * Math.pow(i / sigma, 2))
+    )
+  }
+
+  // range only includes A tokens (X tokens)
+  else if (activeId <= binRange[0] && parsedAmountB.raw.toString() === '0') {
+    const posDelta = binRange[1] - binRange[0] + 1
+    const positiveDeltaIds = Array.from(Array(binRange[1] - activeId).keys())
+      .reverse()
+      .slice(0, posDelta)
+      .reverse()
+      .map((el) => el + 1)
+
+    deltaIds = [...positiveDeltaIds]
+    if (activeId === binRange[0]) {
+      deltaIds.unshift(0)
+    }
+
+    // sigma = R / factor
+    const _R = deltaIds[0]
+    const factor = _R > 20 ? 2 : 1.5
+    const sigma = _R / factor
+
+    // A = 1 / (sigma  * sqrt(2 * pi))
+    const k = Math.pow(Math.PI * 2, -2)
+    const A = k / sigma
+
+    // dist = A * exp(-0.5 * (r /sigma) ^ 2)
+    _distributionX = deltaIds.map(
+      (i) => A * Math.exp(-0.5 * Math.pow(i / sigma, 2))
+    )
+  }
+
+  // range includes both X and Y tokens
+  else {
+    const negDelta = activeId - binRange[0]
+    const posDelta = binRange[1] - activeId
+
+    const negativeDeltaIds = Array.from(Array(negDelta).keys())
+      .reverse()
+      .map((el) => -1 * (el + 1))
+    const positiveDeltaIds = Array.from(Array(posDelta).keys()).map(
+      (el) => el + 1
+    )
+    deltaIds = [...negativeDeltaIds, 0, ...positiveDeltaIds]
+
+    // sigma = R / factor
+    const _R = deltaIds[0]
+    const factor = _R > 20 ? 2 : 1.5
+    const sigma = _R / factor
+
+    // A = 1 / (sigma  * sqrt(2 * pi))
+    const k = Math.pow(Math.PI * 2, -2)
+    const A = k / sigma
+
+    _distributionX = [
+      ...Array(negDelta).fill(0),
+      0,
+      ...positiveDeltaIds.map(
+        (i) => A * Math.exp(-0.5 * Math.pow(i / sigma, 2))
+      )
+    ]
+    _distributionY = [
+      ...negativeDeltaIds.map(
+        (i) => A * Math.exp(-0.5 * Math.pow(i / sigma, 2))
+      ),
+      0,
+      ...Array(posDelta).fill(0)
+    ]
+  }
+
+  // return
   return {
-    deltaIds: [],
-    distributionX: [],
-    distributionY: []
+    deltaIds,
+    distributionX: _distributionX.map((el) => parseEther(el.toString())),
+    distributionY: _distributionY.map((el) => parseEther(el.toString()))
   }
 }
